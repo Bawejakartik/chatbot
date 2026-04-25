a83c2c4e91dec8fbdc8cce898106addb7a597e0e
kartik <kartikbaweja16@gmail.com>
2026-04-25 11:25:06 +0530

    fixing the socket and frontend issue

--- client/src/hooks/UseGetOtherusers.jsx
+++ client/src/hooks/UseGetOtherusers.jsx
@@ -1,7 +1,35 @@
+// import React, { useEffect } from "react";
+// import {useDispatch} from 'react-redux';
+
+
+// import axios from "axios";
+// import { setOtherUsers } from "../redux/usersslice";
+
+// const UseGetOtherusers = () => {
+//   const dispatch = useDispatch();
+
+//   useEffect(() => {
+//     const fetchotherusers = async () => {
+//       try {
+//         axios.defaults.withCredentials = true;
+//         const res = await axios.get(`https://chatbot-rj8b.onrender.com/api/v8/`);
+//         // console.log(res);
+//         //store
+//         // dispatch(setOtherUser(res.data));
+//          dispatch(setOtherUsers(res.data.otheruser || []));
+//       } catch (err) {
+//         console.log(err);
+//       }
+//     };
+//     fetchotherusers();
+//   }, []);
+// };
+
+// export default UseGetOtherusers;
+
+
 import React, { useEffect } from "react";
-import {useDispatch} from 'react-redux';
-
-
+import { useDispatch } from "react-redux";
 import axios from "axios";
 import { setOtherUsers } from "../redux/usersslice";
 
@@ -11,18 +39,27 @@ const UseGetOtherusers = () => {
   useEffect(() => {
     const fetchotherusers = async () => {
       try {
-        axios.defaults.withCredentials = true;
-        const res = await axios.get(`https://chatbot-rj8b.onrender.com/api/v8/`);
-        // console.log(res);
-        //store
-        // dispatch(setOtherUser(res.data));
-         dispatch(setOtherUsers(res.data.otheruser || []));
+        const token = localStorage.getItem("token"); // 🔥 GET TOKEN
+
+        const res = await axios.get(
+          "https://chatbot-rj8b.onrender.com/api/v8/",
+          {
+            headers: {
+              Authorization: `Bearer ${token}`, // 🔥 SEND TOKEN
+            },
+          }
+        );
+
+        dispatch(setOtherUsers(res.data.otheruser || []));
       } catch (err) {
-        console.log(err);
+        console.log("Error fetching users:", err);
       }
     };
+
     fetchotherusers();
   }, []);
+
+  return null;
 };
 
 export default UseGetOtherusers;
